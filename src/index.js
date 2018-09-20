f37851ee694affd74bef0066a1524a40fb74910a
Tony Batty <tonyeb87@gmail.com>
2018-09-20 09:39:25 +0100

    unit testing exercises

--- src/index.js
+++ src/index.js
@@ -1,5 +1,119 @@
-function add(a, b){
+function add(a, b) {
   return a + b;
 }
 
-exports.add = add;
+
+function longestString(strings) {
+  let stringLength = ["", 0]
+  strings.forEach(word => {
+    if (word.length > stringLength[1]) {
+      stringLength[0] = word;
+      stringLength[1] = word.length;
+    }
+  })
+
+  return stringLength[0];
+}
+
+function l337(string) {
+  let stringArr = string.split("")
+  let newArr = stringArr.map(letter => {
+
+    switch (letter) {
+      case "i":
+      case "l":
+        letter = "1";
+        break;
+
+      case "z":
+        letter = 2;
+        break;
+
+      case "e":
+        letter = 3;
+        break;
+
+      case "a":
+        letter = 4;
+        break;
+
+      case "s":
+        letter = 5;
+        break;
+
+      case "g":
+        letter = 6;
+        break;
+
+      case "t":
+      case "y":
+        letter = 7;
+        break;
+
+      case "b":
+        letter = 8;
+        break;
+
+      case "q":
+        letter = 9;
+        break;
+
+      case "o":
+        letter = 0;
+        break;
+
+      default:
+        break;
+
+    }
+    return letter;
+  })
+  return newArr.join("");
+}
+
+function uniqueStrings(string) {
+  const stringObject = {};
+
+  string.forEach(word => {
+    if (stringObject[word]) {
+      stringObject[word] += 1;
+    } else {
+      stringObject[word] = 1;
+    }
+  })
+  let keys = Object.keys(stringObject);
+  const stringArr = [];
+  keys.forEach(key => {
+    if (stringObject[key] === 1) {
+      stringArr.push(key);
+    }
+  })
+  return stringArr;
+}
+
+function Developer(name, languages) {
+  this.name = name;
+  this.languages = languages;
+}
+
+
+
+Developer.prototype.learnLanguage = function (language) {
+  this.languages.push(language);
+  return this.languages
+}
+
+function stringsConcat(arr) {
+  const strArr = arr.map(el => {
+    if (typeof el != "number") {
+       return el;
+      } else {
+        delete el;
+      };
+  console.log(strArr);
+  return strArr;
+})
+
+
+module.exports = { add, longestString, l337, uniqueStrings, Developer, stringsConcat }
+
--- test/index.test.js
+++ test/index.test.js
@@ -5,3 +5,68 @@ test('Addition', function(){
   const result = functions.add(2, 3);
   expect(result).toBe(expected);
 });
+
+test('LongestString',  function(){
+  const expected = "orange";
+  const result = functions.longestString(["blue", "orange"]);
+  expect(result).toBe(expected);
+})
+
+test('LongestString',  function(){
+  const expected = "perpendicular";
+  const result = functions.longestString(["perpendicular", "narrative"]);
+  expect(result).toBe(expected);
+})
+
+test('l337', function(){
+  const expected = "50n681rd"
+  const result = functions.l337("songbird");
+  expect(result).toBe(expected);
+})
+
+test('l337', function(){
+  const expected = "70n7"
+  const result = functions.l337("tony");
+  expect(result).toBe(expected);
+})
+
+test('UniqueStrings',  function(){
+  const expected = ["orange", "blue"];
+  const result = functions.uniqueStrings(["orange", "green", "blue", "green"]);
+  expect(result).toEqual(expected);
+})
+
+test('UniqueStrings',  function(){
+  const expected = ["yellow"];
+  const result = functions.uniqueStrings(["orange", "orange", "green", "blue", "yellow", "blue", "green"]);
+  expect(result).toEqual(expected);
+})
+
+test('developer', function() {
+  const expected = {name: "Tony", languages: ["HTML", "CSS", "Javascript"]}
+  const developer = new functions.Developer("Tony", ["HTML", "CSS", "Javascript"]);
+  expect(developer).toEqual(expected);
+})
+
+test('developer', function() {
+  const expected = {name: "Bob", languages: ["Java", "Python", "C++"]}
+  const developer = new functions.Developer("Bob", ["Java", "Python", "C++"]);
+  expect(developer).toEqual(expected);
+})
+
+test('DevLanguages', function() {
+  const expected = ["HTML", "CSS", "Javascript", "C++"];
+  const developer = new functions.Developer("Tony", ["HTML", "CSS", "Javascript"])
+  const result = developer.learnLanguage("C++");
+  expect(result).toEqual(expected);
+})
+
+test('stringsConcat', function() {
+  const expected = ["redorangeyellowgreenblueindigoviolet"]
+  const result = functions.stringsConcat(["red", 3, "orange", 4, 6, "yellow", 5, 2, "green", "blue", 6, "indigo", 4,  "violet"])
+  expect(result).toEqual(expected);
+})
+
+
+
+
